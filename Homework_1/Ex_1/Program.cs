c75192b1efe6cdd634ad84190f68e34cbed366b2
Sefensi <sefensi_reload@mail.ru>
2023-01-27 01:36:27 +0500

    H2_Ex3

---
+++ Homework_1/Ex_1/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 2
+Console.WriteLine("Введите a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите b");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if (a > b)
+{
+max = a;
+}
+else 
+{
+max = b;
+}
+Console.Write("max = ");
+Console.Write(max);
