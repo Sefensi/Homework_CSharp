493fd27b4497fabf57b87a0e96f5fc0048d5181f
Sefensi <sefensi_reload@mail.ru>
2023-01-27 14:19:11 +0500

    H3_Ex3

--- Homework_3/Ex_3/Program.cs
+++ Homework_3/Ex_3/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 23
+
+/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.*/
+Console.WriteLine("Введите N");
+int number = Convert.ToInt32(Console.ReadLine());
+string massive = "";
+for (int i = 1; i < number + 1; i++)
+{
+    massive += $"{Math.Pow(i, 3)} ";
+}
+Console.WriteLine(massive);
