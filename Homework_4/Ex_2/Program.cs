cb7b54c0d8dca1b83b9afc884e80b98bfeecc7d8
Sefensi <sefensi_reload@mail.ru>
2023-01-29 22:07:32 +0500

    H4_Ex2

---
+++ Homework_4/Ex_2/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12*/
+
+int number_sum(string number)
+{
+    int sum = 0;
+    int bbb = 0;
+    for (int i = 0; i < number.Length; i++)
+    {
+        int.TryParse(Convert.ToString(number[i]), out bbb);
+        sum = sum + bbb;
+        Console.WriteLine(bbb);
+    }
+    return sum;
+}
+Console.WriteLine("Введите число");
+string xxx = Console.ReadLine();
+Console.WriteLine($"Сумма цифр числа равна {number_sum(xxx)}");
+
