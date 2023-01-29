e57b166a7ef81239e083dfa877914fa7b414bbcd
Sefensi <sefensi_reload@mail.ru>
2023-01-29 21:14:10 +0500

    H4_Ex1

--- Homework_3/Ex_3/Program.cs
+++ Homework_3/Ex_3/Program.cs
@@ -1,11 +1,1 @@
-﻿//Задача 23
-
-/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.*/
-Console.WriteLine("Введите N");
-int number = Convert.ToInt32(Console.ReadLine());
-string massive = "";
-for (int i = 1; i < number + 1; i++)
-{
-    massive += $"{Math.Pow(i, 3)} ";
-}
-Console.WriteLine(massive);
+﻿
