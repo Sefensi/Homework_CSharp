8511f1b598fc133a50b1f82d0f8953fdccf0bdbb
Sefensi <sefensi_reload@mail.ru>
2023-02-03 14:32:45 +0500

    H5_Ex1

---
+++ Homework_5/Ex_1/Program.cs
@@ -0,0 +1,45 @@
+﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2*/
+//инициализируем массив
+int[] InitArray(int dimension)
+{
+    int[] arr = new int[dimension];
+    Random rnd = new Random();
+
+    for (int i = 0; i < dimension; i++)
+    {
+        arr[i] = rnd.Next(100, 1000);
+    }
+
+    return arr;
+}
+
+//печатаем массив
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+
+    Console.WriteLine();
+}
+//находим количество четных чисел
+int GetCountOfEvenNumbers(int[] array)
+{
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            count += 1;
+    }
+
+    return count;
+}
+Console.WriteLine("Введите длину массива");
+int[] array = InitArray(Convert.ToInt32(Console.ReadLine()));
+PrintArray(array);
+Console.WriteLine($"Количество четных чисел массива = {GetCountOfEvenNumbers(array)}");
