e29aa9b511c71cfbd7315361fe991bbf5faffdc2
Sefensi <sefensi_reload@mail.ru>
2023-02-03 14:53:34 +0500

    H5_Ex2

---
+++ Homework_5/Ex_2/Program.cs
@@ -0,0 +1,58 @@
+﻿/*Задача 36: 
+Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+
+[-4, -6, 89, 6] -> 0*/
+
+
+//инициализируем массив
+int[] InitArray(int dimension, int from, int to)
+{
+int[] arr = new int[dimension];
+Random rnd = new Random();
+
+for (int i = 0; i < dimension; i++)
+{
+arr[i] = rnd.Next(from, to);
+}
+
+return arr;
+}
+
+
+
+//печатаем массив
+void PrintArray(int[] arr)
+{
+for (int i = 0; i < arr.Length; i++)
+{
+Console.Write($"{arr[i]} ");
+}
+
+Console.WriteLine();
+}
+
+//находим ceмму элементов на нечетных позициях
+int GetNotEvenNumbersSum(int[] array)
+{
+int sum = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+if(array[i] % 2 != 0)
+sum += array[i];
+}
+
+return sum;
+}
+Console.WriteLine("Введите нижню границу чисел массива");
+int ReadFor = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите верхнюю границу чисел массива");
+int ReadTo = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите длину массива");
+int[] array = InitArray(Convert.ToInt32(Console.ReadLine()), ReadFor, ReadTo);
+PrintArray(array);
+Console.WriteLine($"Количество четных чисел массива = {GetNotEvenNumbersSum(array)}");
