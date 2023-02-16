e0b0ae0a50f6bd7103869f2c200fed84cb3a5446
Sefensi <sefensi_reload@mail.ru>
2023-02-16 17:56:57 +0500

    F_Ex1

---
+++ Final/Ex_1/Program.cs
@@ -0,0 +1,44 @@
+﻿/*Задача 64: Задайте значение N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
+
+int GetNaturalNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.Write(message);
+
+        if (int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число или некорректное число. Повторите ввод!");
+        }
+    }
+
+    return result;
+}
+
+void PrintNumber(int N)
+{
+    if (N > 1)
+    {
+        Console.Write($"{N}, ");
+        N--;
+        PrintNumber(N);
+    }
+    else if (N == 1)
+    {
+        Console.Write($"{N} ");
+    }
+}
+
+Console.WriteLine();
+int numbers = GetNaturalNumber("Введите натуральное число: ");
+PrintNumber(numbers);
